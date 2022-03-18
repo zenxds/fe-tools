ed464aa1ed688e36284fbe8ac26b623ca21d03d4
zenxds <zenxds@gmail.com>
2022-03-18 09:38:46 +0800

    feat: buffer不需要utf8

--- src/utils/encode/buffer.ts
+++ src/utils/encode/buffer.ts
@@ -1,7 +1,6 @@
-import * as utf8 from './utf8'
 
 export function encode(str: string): string {
-  const buffer = Array.from(Buffer.from(utf8.encode(str)))
+  const buffer = Array.from(Buffer.from(str))
   return buffer.map(n => n.toString(16)).join(' ')
 }
 
@@ -9,5 +8,5 @@ export function decode(str: string): string {
   const arr = str.split(/\s+/)
   const buffer = arr.map(str => parseInt(str, 16))
 
-  return utf8.decode(Buffer.from(buffer).toString('utf-8'))
+  return Buffer.from(buffer).toString('utf-8')
 }
